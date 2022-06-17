d6834e7bbe937665a88ba31e09fc1fb98870ac69
Alex-Fes <aleksei.fesenko86@gmail.com>
2022-06-17 17:02:21 +0300

    add Callback.tsx

--- src/06-Callback/Callback.tsx
+++ src/06-Callback/Callback.tsx
@@ -1,10 +1,28 @@
+import React, {ChangeEvent, MouseEvent} from "react";
 
 
 export const User = () => {
 
-    return <div>Fesenich
-    <button>Delete</button>
-    <button>Add</button>
+    const deleteUser = (event: MouseEvent<HTMLButtonElement>) => {
+        console.log(event.currentTarget.name)
+    }
+    const addUser = () => {
+        console.log('add')
+    }
+    const onNameChanged = () => {
+        console.log('name changed')
+    }
+    const onLostFocus = () => {
+        console.log('lost focus')
+    }
+    const changeAge = (event: ChangeEvent<HTMLInputElement>) => {
+        console.log('change age' + event.currentTarget.value)
+    }
+
+    return <div><textarea onChange={onNameChanged} onBlur={onLostFocus}>  Fesenich </textarea>
+        <input type="number" name="age" id="" onChange={changeAge}/>
+    <button name='delete' onClick={deleteUser}>Delete</button>
+    <button onClick={addUser}>Add</button>
     </div>
 };
 
